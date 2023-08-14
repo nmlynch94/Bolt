e0ba9431dd2d3cea82eb2d8c2fff72b5357307d7
nmlynch94 <40608755+nmlynch94@users.noreply.github.com>
2023-08-14 18:12:05 -0400

    Update window_launcher_linux.cxx

--- src/browser/window_launcher_linux.cxx
+++ src/browser/window_launcher_linux.cxx
@@ -259,7 +259,6 @@ CefRefPtr<CefResourceRequestHandler> Browser::Launcher::LaunchDeb(CefRefPtr<CefR
 	pid_t pid;
 	posix_spawn_file_actions_init(&file_actions);
 	posix_spawnattr_init(&attributes);
-	posix_spawnattr_setflags(&attributes, POSIX_SPAWN_SETSID);
 	std::string path_str(this->rs3_path.c_str());
 	char* argv[2];
 	argv[0] = path_str.data();
